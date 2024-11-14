0cc81b7487ccbab29efdddc0c443165316a0602b
Angel <alielali.py@gmail.com>
2024-11-14 12:29:18 -0500

    used filtered data in UI, handle keyError

--- weather_backend.py
+++ weather_backend.py
@@ -1,22 +1,18 @@
 import requests
-import pandas as pd
 
 API_KEY = "cf573dabb5997768e8ef59feb0920e2a"
 
-def get_data(place, forcast_days=None, kind=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+
+def get_data(place, forcast_days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}" \
+          f"&appid={API_KEY}"
     responds = requests.get(url)
     data = responds.json()
     filtered_data = data['list']
     nr_values = 8 * forcast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dic['main']['temp'] for dic in filtered_data]
-    if kind == 'Sky':
-        filtered_data = [dic['weather'][0]['main'] for dic in filtered_data]
     return filtered_data
 
 
-
 if __name__ == "__main__":
-    print(get_data('canada', 2, 'Sky'))
+    print(get_data('canada', 2))
--- weather_ui.py
+++ weather_ui.py
@@ -1,19 +1,49 @@
 import streamlit as st
 import plotly.express as px
+from weather_backend import get_data
 
 
 st.title('Weather Forcast for the Next Days')
 place = st.text_input("Place")
 days = st.slider('Forcast Days', min_value=1, max_value=5,
-                 help='Select the number of forecasted days' )
+                 help='Select the number of forecasted days')
 options = st.selectbox("Select data to view", ['Temperature', 'Sky'])
 
 st.subheader(f'{options} for the next {days} days in {place}')
 
-date = ['2024-07-01', '2024-07-02', '2024-07-03']
 
-temperature = [16, 19, 21]
+if place:
+    try:
 
-figure = px.line(x=date, y=temperature, labels={"x": 'Date', 'y': 'Temperature (C)'})
+        # call the data
+        filtered_data = get_data(place, days)
 
-st.plotly_chart(figure)
+        if options == 'Temperature':
+            temperature = [dic['main']['temp'] for dic in filtered_data]
+            date = [dic['dt_txt'] for dic in filtered_data]
+            figure = px.line(x=date, y=temperature,
+                             labels={"x": 'Date', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
+
+        if options == 'Sky':
+            images = {'Clouds': 'images/cloud.png', 'Clear': 'images/clear.png',
+                      'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+            sky_condition = [dic['weather'][0]['main'] for dic in filtered_data]
+
+            image_paths = [images[condition] for condition in sky_condition]
+
+            # Create columns for a grid-like display
+            # I use 8 columns because we have 8 observations per day
+            columns_per_row = 8
+
+            # Split the list of images into chunks of columns_per_row
+            rows = [image_paths[i:i + columns_per_row] for i in
+                    range(0, len(image_paths), columns_per_row)]
+
+            # Display images row by row
+            for row in rows:
+                cols = st.columns(columns_per_row)
+                for col, image in zip(cols, row):
+                    col.image(image, width=115)
+    except KeyError:
+        st.info("It looks like you've entered an invalid country name. ")
